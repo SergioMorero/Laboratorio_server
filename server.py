dbcf4c7608c94cf779626c03b404d8d10d2b57e4
SergioMorero <smorero@mail.austral.edu.ar>
2025-07-28 16:14:13 -0300

    aaaaaaaaa

--- server.py
+++ server.py
@@ -670,19 +670,16 @@ def google_login():
         "redirect_uri": "https://jumping-pals.onrender.com/googlelogin",
         "grant_type": "authorization_code"
     })
-    session_status[session_id] = 1
-
     token_response = response.json()
     print(token_response)  # para depurar
-
     id_token = token_response.get("id_token")
     if not id_token:
         return {"error": "No se recibió id_token", "detalle": token_response}, 400
-
     user_info = decode_id_token(response.json()['id_token'])
     user_name = user_info.get('name')
     user_email = user_info.get("email")
     session_info[session_id] = [user_name[:10], user_email]
+    session_status[session_id] = 1
     #return redirect(f"https://jumping-pals.onrender.com/")
     return response.json()
 
